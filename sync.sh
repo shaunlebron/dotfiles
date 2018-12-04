2fce985548e29ac6a042eeaa9f1cff6f08d4b75a
Shaun Williams <shaun.e.williams@gmail.com>
2018-12-04 11:01:56 -0600

    remove unused

--- sync.sh
+++ sync.sh
@@ -6,7 +6,3 @@ DOTDIR=$(pwd)
 cd ~
 ln -s $DOTDIR/bash_profile .bash_profile
 ln -s $DOTDIR/vimrc .vimrc
-ln -s $DOTDIR/screenrc .screenrc
-ln -s $DOTDIR/tmux.conf .tmux.conf
-ln -s $DOTDIR/ctags .ctags
-ln -s $DOTDIR/lein_profiles.clj .lein/profiles.clj
